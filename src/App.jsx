f26ce935229c5dadce6bc5e0cf9e74c196f8f296
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-09-21 14:27:29 +0530

    commit1 declare components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css"
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>placeholder
-        
-      </div>
+ 
+            <div className="app-container">
+                   
+                    <Sidebar></Sidebar>
+                            <div className="content"> 
+                            <Header></Header>
+                            <CreatePost></CreatePost>
+                            <Footer></Footer>
+                            </div>
+            </div>
+     
     </>
   )
 }
---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,31 @@
+const CreatePost = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Add your form submission logic here
+    };
+
+    return (
+        <>
+            <div className="bd-example m-0 border-0">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default CreatePost;
