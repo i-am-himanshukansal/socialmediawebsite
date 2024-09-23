28c3f1afd7f96daf8f966a484e5f65c2d3f3fd5d
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-09-23 22:26:05 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ const Sidebar = ({selectedTab,setselectedtab}) => {
       <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{ width: '280px' }}>
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
           <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-          <span className="fs-4">Sidebar</span>
+          <span className="fs-4">SOCIAL MEDIA </span>
         </a>
         <hr />
                   <ul className="nav nav-pills flex-column mb-auto">
